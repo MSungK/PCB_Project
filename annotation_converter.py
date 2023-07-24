4bae9c1f845fa43de3e52dee6d81c1440043c9ce
Chris <ambushe1801@gmail.com>
2023-07-24 02:53:37 +0000

    Add Trainsetinfo

--- annotation_converter.py
+++ annotation_converter.py
@@ -10,10 +10,25 @@ def converter(norm, x1, y1, x2, y2):
     return x_center, y_center, width, height
 
 
+def make_dict(trainset_info):
+    is_train = dict()
+    with open(trainset_info, 'r') as reader:
+        lines = reader.readlines()
+    for line in lines:
+        file1, file2 = line.split(' ')
+        is_train[file1] = True
+        is_train[file2] = True
+    return is_train
+
+
 if __name__ == '__main__': 
     path = 'dataset/DeepPCB/PCBData/'
     label_path = 'dataset/DeepPCB/labels/'
     image_path = 'dataset/DeepPCB/images/'
+
+    trainset_info = 'dataset/DeepPCB/PCBData/trainval.txt'
+    is_train = make_dict(trainset_info)
+    
     dirs = os.listdir(path)
     cnt = 0
 
